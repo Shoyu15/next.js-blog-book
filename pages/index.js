02e07133532cb2a7e3e614cf3e31cad424dd460f
sho haruki <shoharuki@shonoMacBook-Pro.local>
2023-07-06 19:14:58 +0900

    done until chapter 4

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Hero from "components/hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero 
+        title="TipsBlog"
+        subtitle="作って学ぶアウトプットブログ"
+        imageOn
+      />
+    </>
+  );
+}
