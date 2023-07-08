02b13a7b303fe17e629361fbe06e87b6411232f0
sho haruki <shoharuki@shonoMacBook-Pro.local>
2023-07-08 11:42:53 +0900

    done chapter8

---
+++ components/post-categories.js
@@ -0,0 +1,22 @@
+import { faFolderOpen } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import styles from "styles/post-categories.module.css";
+
+export default function PostCategories({ categories }) {
+  return (
+    <div className={styles.flexContainer}>
+      <h3 className={styles.heading}>
+        <FontAwesomeIcon icon={faFolderOpen} />
+        <span className="sr-only">Categories</span>
+      </h3>
+      <ul className={styles.liost}>
+        {categories.map(({ name, slug }) => (
+          <li key={slug}>
+            <Link href={`/blog/category/${slug}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,5 +1,17 @@
 import Container from "@/components/container";
+import ConvertBody from "@/components/convert-body";
+import Meta from "@/components/meta";
+import PostBody from "@/components/post-body";
+import PostCategories from "@/components/post-categories";
+import PostHeader from "@/components/post-header";
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar,
+} from "@/components/two-column";
 import { getPostBySlug } from "lib/api";
+import { extractText } from "lib/extract-text";
+import Image from "next/legacy/image";
 
 export default function Schedule({
   title,
@@ -7,10 +19,45 @@ export default function Schedule({
   content,
   eyecatch,
   categories,
+  description,
 }) {
   return (
     <Container>
-      <h1>{title}</h1>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      <article>
+        <PostHeader
+          title={title}
+          subtitle="サブタイトルー!!"
+          publish={publish}
+        />
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=""
+            layout="responsive"
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes="(min-width:1152px) 1152px, 100vw"
+            priority
+          />
+        </figure>
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
     </Container>
   );
 }
@@ -18,7 +65,7 @@ export default function Schedule({
 export async function getStaticProps() {
   const slug = "schedule";
   const post = await getPostBySlug(slug);
-
+  const description = extractText(post.content);
   return {
     props: {
       title: post.title,
@@ -26,6 +73,7 @@ export async function getStaticProps() {
       content: post.content,
       eyecatch: post.eyecatch,
       categories: post.categories,
+      description: description,
     },
   };
 }
